fccb322c3ce24fc8b818e5d6768ad597127a975f
Jonas Riishede <jonas2635@gmail.com>
2017-08-28 08:37:01 +0200

    Frontend baseviewmodel

---
+++ Pauls ølstue/Frontend/ViewModels/BaseViewmodel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.ViewModels
+{
+    public class BaseViewmodel
+    {
+        protected readonly string ConnectionString;
+
+        public BaseViewmodel()
+        {
+            ConnectionString = "Server=35.195.31.100;Database=PaulsData;Uid=root;Pwd=password;";
+        }
+
+    }
+}
