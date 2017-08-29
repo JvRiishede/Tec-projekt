aba4d20d487bbfcdac4c9eefdd1c83e5d19ea6d6
Eruk347 <kristian_kure@yahoo.dk>
2017-08-29 15:41:34 +0200

    Frontend dynamisk layout
    Frontenden er sat op så der oprettes en textbox med navn fra vare databasen.

--- Pauls ølstue/Frontend/Views/UserContent.xaml.cs
+++ Pauls ølstue/Frontend/Views/UserContent.xaml.cs
@@ -12,6 +12,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ViewModels;
+using Data;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,10 +27,17 @@ namespace Frontend.Views
     /// </summary>
     public sealed partial class UserContent : Page
     {
+        private List<Vare> vare;
         public List<Test2> DataList { get; set; }
         public UserContent()
         {
             this.InitializeComponent();
+            UCVM = new UserContentViewModel();
+            UCVM.Load();
+
+            vare = VareManager.Varer();
+            Debug.WriteLine(vare[0].VareNavn);
+
             DataList = new List<Test2>
             {
                 new Test2
@@ -40,11 +52,18 @@ namespace Frontend.Views
                 }
             };
         }
-
+        public UserContentViewModel UCVM { get; set; }
         public class Test2
         {
             public string Name { get; set; }
             public int Amount { get; set; }
+        }
+
+        private void Products_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            
+            //DataList.Add(new Test2 { Name=e.ClickedItem.ToString()})
+            Debug.WriteLine(" ");
         }
     }
 }
