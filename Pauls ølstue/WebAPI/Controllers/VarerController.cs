dd63547254d8c15cf86676d0804ace553ce230e0
Eruk347 <kristian_kure@yahoo.dk>
2017-09-04 20:41:08 +0200

    Varercontroller

--- Pauls ølstue/WebAPI/Controllers/VarerController.cs
+++ Pauls ølstue/WebAPI/Controllers/VarerController.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Data.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Model;
 
 namespace WebAPI.Controllers
 {
     public class VarerController : ApiController
     {
+        private readonly IUserService _userService;
+        public VarerController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
     }
 }
