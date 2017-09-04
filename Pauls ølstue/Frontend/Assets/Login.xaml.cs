82c2643542b04c42d8f6bef7b03df3361c1a483c
Eruk347 <kristian_kure@yahoo.dk>
2017-09-04 14:07:13 +0200

    API

--- Pauls ølstue/Frontend/Assets/Login.xaml.cs
+++ Pauls ølstue/Frontend/Assets/Login.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Data;
+using MySql.Data.MySqlClient;
+
 
 // The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,6 +29,23 @@ namespace Frontend.Assets
 
         private void LoginProcess()
         {
+            string password = Password.Text;
+            string username = Brugernavn.Text;
+            string passwordHash;
+
+            var dbConn =DBConnection.Instance();
+            if (dbConn.IsConnect())
+            {
+                string query = "Select KodeHash FROM Bruger where VærelseNr='"+username+"';";
+                var cmd = new MySqlCommand(query, dbConn.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    passwordHash=reader.GetString(0);
+                    //brugere.Add(reader.GetInt32(2).ToString() + ", " + reader.GetString(0) + " " + reader.GetString(1));
+                }
+                dbConn.Close();
+            }
 
         }
 
@@ -37,6 +56,7 @@ namespace Frontend.Assets
 
         private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
+            Application.Current.Exit();
         }
     }
 }
