491511b31d32f756d21ca9193c3fef56bab93658
Jonas Riishede <jonas2635@gmail.com>
2017-09-15 12:58:46 +0200

    Frontend antal valgte vare opdateres nu korrekt i view

--- Pauls ølstue/Frontend/Data/OrderItem.cs
+++ Pauls ølstue/Frontend/Data/OrderItem.cs
@@ -1,21 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Frontend.Annotations;
 
 namespace Data
 {
-    public class OrderItem
+    public class OrderItem : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int Amount { get; set; }
         public bool ErDrink { get; set; }
-        public string Full { get; set; }
+
+        private string _name;
+
+        public string Full
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Full));
+            }
+        }
+
         public void Combine()
         {
             Full = Name + "   " + "Antal: " + Amount.ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
